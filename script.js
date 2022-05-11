1bd90ffad0712756e9c2ac611f9889dcf41fa42e
Sandra Martos <bysmartos@gmail.com>
2022-05-11 21:35:27 +0200

    html con id en h1 y ol en songs container

--- script.js
+++ script.js
@@ -3,6 +3,36 @@
 
 //Sandra escribe debajo de esta linea 
 
+// document.addEventListener("click", colorear)
+
+// function colorear(){
+//     document.querySelector(".navMusicStyle").style.backgroundColor="pink"
+// }
+  
+    fetch("music.json")
+    .then(res => res.json())
+    .then(data=>console.log(data));
+
+// window.addEventListener("load", colorear)
+
+// function colorear(){
+//     document.querySelector(".navMusicStyle").style.backgroundColor="pink"
+// }
+    
+window.addEventListener("load", cargarJSON);
+
+function cargarJSON(){
+    fetch("music.json")
+     .then(res => res.json())
+     .then(function(data){
+         let html="";
+         data.forEach(song => {
+             html += `<li>${song.name}</li>`
+         });
+         document.querySelector(".songs").innerHTML=html;
+     });
+}
+
 
 //Sara escribe debajo de esta linea
 
