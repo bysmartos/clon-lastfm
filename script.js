2f14cb4fad0cf884fad5ef3a390964d1355efdc3
Sandra Martos <bysmartos@gmail.com>
2022-05-12 01:29:17 +0200

    user story 1 terminada

--- script.js
+++ script.js
@@ -2,24 +2,42 @@
 
 
 //Sandra escribe debajo de esta linea 
+//Las proximas tres lienas comentadas son de test para combrobar
+// que funciona bien el docuemtno js
 
-// document.addEventListener("click", colorear)
+    // document.addEventListener("click", colorear)
 
-// function colorear(){
-//     document.querySelector(".navMusicStyle").style.backgroundColor="pink"
-// }
-  
+    // function colorear(){
+    //     document.querySelector(".navMusicStyle").style.backgroundColor="pink"
+    // }
+  // Esto es simplemente para ver el array en consola por si lo queremos consultar
     fetch("music.json")
     .then(res => res.json())
     .then(data=>console.log(data));
 
-// window.addEventListener("load", colorear)
 
-// function colorear(){
-//     document.querySelector(".navMusicStyle").style.backgroundColor="pink"
-// }
+//esto es otro test para ver si el window....load.. funciona.
+    // window.addEventListener("load", colorear)
+
+    // function colorear(){
+    //     document.querySelector(".navMusicStyle").style.backgroundColor="pink"
+    // }
     
+ //USER STORY 1: se carga el listado JSON  con todas las canciones al cargar la pagina  
 window.addEventListener("load", cargarJSON);
+//Esto funciona, pero lo he copiado debajo para probar cosas y de momento funciona bien, me guardo este comentado por si lo necesito mas adelnate
+
+// function cargarJSON(){
+//     fetch("music.json")
+//      .then(res => res.json())
+//      .then(function(data){
+//          let html="";
+//          data.forEach(song => {
+//              html += `<li>${song.artist.name} ${song.name} ${song.listeners} listeners</li>`
+//          });
+//          document.querySelector(".songs").innerHTML=html;
+//      });
+// }
 
 function cargarJSON(){
     fetch("music.json")
@@ -27,11 +45,18 @@ function cargarJSON(){
      .then(function(data){
          let html="";
          data.forEach(song => {
-             html += `<li>${song.name}</li>`
+             html += `<li><img src="https://img2.freepng.es/20180827/ze/kisspng-computer-icons-clip-art-portable-network-graphics-icons-for-free-glyphpack-5b8492aa350d03.4427377915354149542173.jpg" atr="" ><span class="songArtist" >  ${song.artist.name}</span> <span class="songBold" >${song.name}</span><span class="listEnd"> ${song.listeners} listeners</span></li>`
          });
          document.querySelector(".songs").innerHTML=html;
+         
      });
 }
+
+
+
+
+
+
 
 
 //Sara escribe debajo de esta linea
