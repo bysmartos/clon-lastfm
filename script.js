6499045bb48f876dc7ce27c3680f5cce2f89f063
Sandra Martos <bysmartos@gmail.com>
2022-05-12 14:24:57 +0200

    user story 1,4 y 6 done

--- script.js
+++ script.js
@@ -23,6 +23,10 @@
     //     document.querySelector(".navMusicStyle").style.backgroundColor="pink"
     // }
     
+
+
+
+    
  //USER STORY 1: se carga el listado JSON  con todas las canciones al cargar la pagina  
 window.addEventListener("load", cargarJSON);
 //Esto funciona, pero lo he copiado debajo para probar cosas y de momento funciona bien, me guardo este comentado por si lo necesito mas adelnate
@@ -52,10 +56,43 @@ function cargarJSON(){
      });
 }
 
+ //USER STORY 4: Cuando clico en la opción "Rock" de la barra superior Entonces puedo ver un listado con las canciones más escuchadas del género rock
+ let rock = document.querySelector(".rock")
+ rock.addEventListener("click", listaRock);
 
+ function listaRock(){
+    fetch("music.json")
+    .then(res => res.json())
+    .then(function(data){
+        let html="";
+        let rockArray= data.filter(song => song.genres.includes("rock") )
+        rockArray.forEach(song => {
+            html += `<li><img src="https://img2.freepng.es/20180827/ze/kisspng-computer-icons-clip-art-portable-network-graphics-icons-for-free-glyphpack-5b8492aa350d03.4427377915354149542173.jpg" atr="" ><span class="songArtist" >  ${song.artist.name}</span> <span class="songBold" >${song.name}</span><span class="listEnd"> ${song.listeners} listeners</span></li>`
+        });
+        document.querySelector(".songs").innerHTML=html;
+        })
+        
+    };
 
+    
+ //USER STORY 6: Cuando clico en la opción "Rock" de la barra superior Entonces puedo ver un listado con las canciones más escuchadas del género rock
+ let indie = document.querySelector(".indie")
+ indie.addEventListener("click", listaIndie);
 
-
+ function listaIndie(){
+    fetch("music.json")
+    .then(res => res.json())
+    .then(function(data){
+        let html="";
+        let indieArray= data.filter(song => song.genres.includes("indie") )
+        indieArray.forEach(song => {
+            html += `<li><img src="https://img2.freepng.es/20180827/ze/kisspng-computer-icons-clip-art-portable-network-graphics-icons-for-free-glyphpack-5b8492aa350d03.4427377915354149542173.jpg" atr="" ><span class="songArtist" >  ${song.artist.name}</span> <span class="songBold" >${song.name}</span><span class="listEnd"> ${song.listeners} listeners</span></li>`
+        });
+        document.querySelector(".songs").innerHTML=html;
+        })
+        
+    };
+    
 
 
 
