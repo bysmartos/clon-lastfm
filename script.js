248409a4e9f598c970f92569067f6c40e3cafa2c
Sandra Martos <bysmartos@gmail.com>
2022-05-19 10:57:49 +0200

    ultimos cambios

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-//IMPORT FUNCIONES DE testing.js
-import {filtrarEstilo} from "./testing.js"
+//RECORDATORIO: ID PONIENDO COMENTARIOS EN VUESTRA PARTE SOBRE LO QUE ESTAIS HACIENDO
+
+// //Funcion test suma
+
+// export const sumar = (a,b) => {
+
+//     if(a===0 || b===0){
+//         return 0;
+//     } else{
+//   return  a+b
+// }
+// };
 
 //Sandra escribe debajo de esta linea 
 //Esto es para ver el array en la consola
@@ -26,16 +36,15 @@ function cargarJSON(){
 }
 
 // FUNCION PARA FILTRO ESTILO MUSICAL
-// function filtrarEstilo(array,estilo,titulo){
-//     let html="";
-//     let rockArray= array.filter(song => song.genres.includes(estilo) )
-//     rockArray.forEach(song => {
-//         html += `<li><img src="logoMusica.png" atr=""/ ><span class="songArtist" > <a href=${song.artist.url}> ${song.artist.name} </a> </span> <span class="songBold"> <a href=${song.url}> ${song.name} </a> </span><span class="listEnd"> ${song.listeners} listeners</span></li>`
-//     });
-//     document.querySelector(".songs").innerHTML=html;
-//     document.querySelector("#titleSongsContainer").innerHTML=titulo;
-//     }
-
+function filtrarEstilo(array,estilo,titulo){
+    let html="";
+    let rockArray= array.filter(song => song.genres.includes(estilo) )
+    rockArray.forEach(song => {
+        html += `<li><img src="logoMusica.png" atr=""/ ><span class="songArtist" > <a href=${song.artist.url}> ${song.artist.name} </a> </span> <span class="songBold"> <a href=${song.url}> ${song.name} </a> </span><span class="listEnd"> ${song.listeners} listeners</span></li>`
+    });
+    document.querySelector(".songs").innerHTML=html;
+    document.querySelector("#titleSongsContainer").innerHTML=titulo;
+    }
  //USER STORY 4: Cuando clico en la opción "Rock" de la barra superior Entonces puedo ver un listado con las canciones más escuchadas del género rock
  let rock = document.querySelector(".rock")
  rock.addEventListener("click", listaRock);
