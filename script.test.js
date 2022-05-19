bca9fa89cd4921d1a910c82e377d10db0c13f0b1
Sandra Martos <bysmartos@gmail.com>
2022-05-19 08:13:36 +0200

    testing quitando las pruebas de las funciones de clon

--- script.test.js
+++ script.test.js
@@ -1,17 +1,12 @@
-import {sumar, filtrarEstilo} from "./testing"
+import {sumar} from "./testing"
+
 
 test ("1+1=2", () =>{
     expect (1+1).toBe(2);
 })
 
-test("sumar should return when arguments are 1 and 2", ()=>{
+test("sumar should return 3 when arguments are 1 and 2", ()=>{
     const resultado = sumar(1,2)
     expect(resultado).toBe(3);
 
 })
-
-// test("filtrarEstilo retorna", ()=>{
-//     const resultado = sumar(1,2)
-//     expect(resultado).toBe(3);
-
-// })
--- testing.js
+++ testing.js
@@ -8,12 +8,3 @@ export const sumar = (a,b) => {
   return  a+b
 }
 };
-export function filtrarEstilo(array,estilo,titulo){
-    let html="";
-    let rockArray= array.filter(song => song.genres.includes(estilo) )
-    rockArray.forEach(song => {
-        html += `<li><img src="logoMusica.png" atr=""/ ><span class="songArtist" > <a href=${song.artist.url}> ${song.artist.name} </a> </span> <span class="songBold"> <a href=${song.url}> ${song.name} </a> </span><span class="listEnd"> ${song.listeners} listeners</span></li>`
-    });
-    document.querySelector(".songs").innerHTML=html;
-    document.querySelector("#titleSongsContainer").innerHTML=titulo;
-    }
